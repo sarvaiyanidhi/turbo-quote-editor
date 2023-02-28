8e4942068fd38e94f4213070bea8d8255fda020e
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-02-27 22:36:00 -0500

    Updated test to follow turbo frame code update

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -9,10 +9,13 @@ class QuotesTest < ApplicationSystemTestCase
   test "Create New Quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
+
     click_on "New Quote"
-    assert_selector "h1", text: "New quote"
     fill_in "Name", with: "Capybara Quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
+
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara Quote"
   end
@@ -26,10 +29,13 @@ class QuotesTest < ApplicationSystemTestCase
   test "Updating Quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
+
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
     fill_in "Name", with: "Updated Quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
+
     assert_selector "h1", text: "Quotes"
     assert_text "Updated Quote"
   end
@@ -37,6 +43,7 @@ class QuotesTest < ApplicationSystemTestCase
   test "Deleting Quote" do
     visit quotes_path
     assert_text @quote.name
+    
     click_on "Delete", match: :first
     assert_no_text @quote.name
   end
